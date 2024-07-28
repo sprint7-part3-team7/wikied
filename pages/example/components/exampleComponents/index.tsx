1c2fe2449566765393637f567f376add816733cc
binyongbin <kyb2475@gmail.com>
2024-07-28 13:05:18 +0900

    chore: 프로젝트 초기 세팅 완료

---
+++ pages/example/components/exampleComponents/index.tsx
@@ -0,0 +1,11 @@
+import styles from "@/pages/example/components/exampleComponents/styles.module.scss";
+
+const ExampleComponents = () => {
+  return (
+    <>
+      <p className={styles["wohaha"]}>우하하</p>
+    </>
+  );
+};
+
+export default ExampleComponents;
