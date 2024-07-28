01cf3b3fb4bac9f9f3333432ed4ea13938cc5af5
binyongbin <kyb2475@gmail.com>
2024-07-28 13:07:56 +0900

    chore: .env 파일 추가 및 서버에서 받아오는 이미지 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /* 추후 API 연동 후 설정 */
+  /*
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/Sprint_Mission/**",
+      },
+    ],
+  },
+  */
 };
 
 export default nextConfig;
