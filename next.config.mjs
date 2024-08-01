40563e03623df8c7c73478ca80838cfbd5c1bc8a
binyongbin <kyb2475@gmail.com>
2024-08-01 13:02:45 +0900

    chore: clsx, svgr 라이브러리 설치

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,21 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
-      "example.com",
+      'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+      'example.com',
     ],
   },
+  /**
+   * @svgr/webpack 사용하기 위해 추가해야하는 설정
+   */
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ['@svgr/webpack'],
+  //   });
+
+  //   return config;
+  // },
 };
 
 export default nextConfig;
