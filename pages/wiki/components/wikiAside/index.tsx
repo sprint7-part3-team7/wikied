ed6b758b5b181fd0ad59214def9c94a6f105e3ea
hayuri1990 <hayuri1990@gmail.com>
2024-08-01 10:36:56 +0900

    feat: wiki-aside 컴포넌트 반응형 수정 및 토글 기능 추가

--- pages/wiki/components/wikiAside/index.tsx
+++ pages/wiki/components/wikiAside/index.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-// import { getProfiles } from '@/services/api/wiki';
+import React, { useState } from 'react';
+import Image from 'next/image';
 import { Profile } from '@/types/wiki';
 import styles from '@/pages/wiki/components/wikiAside/styles.module.scss';
-import Image from 'next/image';
+import expandIcon from '@/assets/icons/ic_expand.svg';
 
 interface WikiAsideProps {
   className?: string;
@@ -10,6 +10,12 @@ interface WikiAsideProps {
 }
 
 const WikiAside: React.FC<WikiAsideProps> = ({ className, profile }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setIsExpanded((prevState) => !prevState);
+  };
+
   return (
     <div className={`${styles['user-profile']} ${className}`}>
       <div className={styles['image-container']}>
@@ -22,43 +28,94 @@ const WikiAside: React.FC<WikiAsideProps> = ({ className, profile }) => {
         />
       </div>
       <div className={styles['user-attribute-container']}>
-        <div className={styles['user-attribute']}>
-          <span className={styles['attribute-name']}>거주 도시</span>
-          <span className={styles['attribute-value']}>{profile?.city}</span>
+        <div className={styles['always-show-user-attribute']}>
+          <div className={styles['user-attribute']}>
+            <span className={styles['attribute-name']}>거주 도시</span>
+            <span className={styles['attribute-value']}>{profile?.city}</span>
+          </div>
+          <div className={styles['user-attribute']}>
+            <span className={styles['attribute-name']}>MBTI</span>
+            <span className={styles['attribute-value']}>{profile?.mbti}</span>
+          </div>
+          <div className={styles['user-attribute']}>
+            <span className={styles['attribute-name']}>직업</span>
+            <span className={styles['attribute-value']}>{profile?.job}</span>
+          </div>
         </div>
-        <div className={styles['user-attribute']}>
-          <span className={styles['attribute-name']}>MBTI</span>
-          <span className={styles['attribute-value']}>{profile?.mbti}</span>
+        {/* 데스크탑 */}
+        <div className={styles['desktop-user-attribute']}>
+          <div className={styles['user-attribute']}>
+            <span className={styles['attribute-name']}>SNS 계정</span>
+            <span className={styles['attribute-value']}>{profile?.sns}</span>
+          </div>
+          <div className={styles['user-attribute']}>
+            <span className={styles['attribute-name']}>생일</span>
+            <span className={styles['attribute-value']}>
+              {profile?.birthday}
+            </span>
+          </div>
+          <div className={styles['user-attribute']}>
+            <span className={styles['attribute-name']}>별명</span>
+            <span className={styles['attribute-value']}>
+              {profile?.nickname}
+            </span>
+          </div>
+          <div className={styles['user-attribute']}>
+            <span className={styles['attribute-name']}>혈액형</span>
+            <span className={styles['attribute-value']}>
+              {profile?.bloodType}
+            </span>
+          </div>
+          <div className={styles['user-attribute']}>
+            <span className={styles['attribute-name']}>국적</span>
+            <span className={styles['attribute-value']}>
+              {profile?.nationality}
+            </span>
+          </div>
         </div>
-        <div className={styles['user-attribute']}>
-          <span className={styles['attribute-name']}>직업</span>
-          <span className={styles['attribute-value']}>{profile?.job}</span>
-        </div>
-        <div className={styles['user-attribute']}>
-          <span className={styles['attribute-name']}>SNS 계정</span>
-          <span className={styles['attribute-value']}>{profile?.sns}</span>
-        </div>
-        <div className={styles['user-attribute']}>
-          <span className={styles['attribute-name']}>생일</span>
-          <span className={styles['attribute-value']}>{profile?.birthday}</span>
-        </div>
-        <div className={styles['user-attribute']}>
-          <span className={styles['attribute-name']}>별명</span>
-          <span className={styles['attribute-value']}>{profile?.nickname}</span>
-        </div>
-        <div className={styles['user-attribute']}>
-          <span className={styles['attribute-name']}>혈액형</span>
-          <span className={styles['attribute-value']}>
-            {profile?.bloodType}
-          </span>
-        </div>
-        <div className={styles['user-attribute']}>
-          <span className={styles['attribute-name']}>국적</span>
-          <span className={styles['attribute-value']}>
-            {profile?.nationality}
-          </span>
-        </div>
+        {/* 데스크탑 */}
+        {isExpanded && (
+          <div className={styles['selective-show-user-attribute']}>
+            <div className={styles['user-attribute']}>
+              <span className={styles['attribute-name']}>SNS 계정</span>
+              <span className={styles['attribute-value']}>{profile?.sns}</span>
+            </div>
+            <div className={styles['user-attribute']}>
+              <span className={styles['attribute-name']}>생일</span>
+              <span className={styles['attribute-value']}>
+                {profile?.birthday}
+              </span>
+            </div>
+            <div className={styles['user-attribute']}>
+              <span className={styles['attribute-name']}>별명</span>
+              <span className={styles['attribute-value']}>
+                {profile?.nickname}
+              </span>
+            </div>
+            <div className={styles['user-attribute']}>
+              <span className={styles['attribute-name']}>혈액형</span>
+              <span className={styles['attribute-value']}>
+                {profile?.bloodType}
+              </span>
+            </div>
+            <div className={styles['user-attribute']}>
+              <span className={styles['attribute-name']}>국적</span>
+              <span className={styles['attribute-value']}>
+                {profile?.nationality}
+              </span>
+            </div>
+          </div>
+        )}
       </div>
+      <button className={styles['expand-btn']} onClick={handleToggle}>
+        <Image
+          className={styles['expand-icon']}
+          src={expandIcon}
+          width={24}
+          height={24}
+          alt='profile image'
+        />
+      </button>
     </div>
   );
 };
