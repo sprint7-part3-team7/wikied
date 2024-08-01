8038e9d404fc7816af795efc82f866f14f453b8d
binyongbin <kyb2475@gmail.com>
2024-08-01 13:05:28 +0900

    refactor: 버튼 컴포넌트 적용 및 코드 포맷팅

--- pages/mypage/components/changePasswordInput/index.tsx
+++ pages/mypage/components/changePasswordInput/index.tsx
@@ -1,37 +1,38 @@
-import styles from "@/pages/mypage/components/changePasswordInput/styles.module.scss";
+import Button from '@/components/button';
+import styles from '@/pages/mypage/components/changePasswordInput/styles.module.scss';
 
 const ChangePasswordInput = () => {
   return (
-    <form className={styles["container"]}>
-      <label htmlFor="password" className={styles["label"]}>
+    <form className={styles['container']}>
+      <label htmlFor="password" className={styles['label']}>
         비밀번호 변경
       </label>
-      <div className={styles["password-input-wrapper"]}>
+      <div className={styles['password-input-wrapper']}>
         <input
           id="password"
           name="currentPassword"
           type="password"
-          className={styles["password-input"]}
+          className={styles['password-input']}
           placeholder="기존 비밀번호"
         />
         <input
           id="password"
           name="newPassword"
           type="password"
-          className={styles["password-input"]}
+          className={styles['password-input']}
           placeholder="새 비밀번호"
         />
         <input
           id="password"
           name="verifyNewPassword"
           type="password"
-          className={styles["password-input"]}
+          className={styles['password-input']}
           placeholder="새 비밀번호 확인"
         />
       </div>
-      <button type="submit" className={styles["button"]}>
+      <Button color="primary" size="small" defaultPadding alignEnd>
         변경하기
-      </button>
+      </Button>
     </form>
   );
 };
