64ca177b0ab253fa5a259318eb8493a0bbb95347
hayuri1990 <hayuri1990@gmail.com>
2024-08-03 21:41:08 +0900

    refactor: 코드리뷰 수정사항 반영

--- pages/wiki/[code]/components/wikiHeader/index.tsx
+++ pages/wiki/[code]/components/wikiHeader/index.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image";
-import Link from "next/link";
-import { Profile, Section } from "@/types/wiki";
-import styles from "@/pages/wiki/[code]/components/wikiHeader/styles.module.scss";
-import link from "@/assets/icons/ic_link.svg";
-import bold from "@/assets/icons/ic_bold.svg";
-import italic from "@/assets/icons/ic_italic.svg";
-import underline from "@/assets/icons/ic_underline.svg";
-import arrow from "@/assets/icons/ic_arrow.svg";
-import bullet from "@/assets/icons/ic_bullet.svg";
-import number from "@/assets/icons/ic_number.svg";
-import align from "@/assets/icons/ic_align.svg";
-import image from "@/assets/icons/ic_image.svg";
-import video from "@/assets/icons/ic_video.svg";
+import Image from 'next/image';
+import Link from 'next/link';
+import { Profile, Section } from '@/types/wiki';
+import styles from '@/pages/wiki/[code]/components/wikiHeader/styles.module.scss';
+import link from '@/assets/icons/ic_link.svg';
+import bold from '@/assets/icons/ic_bold.svg';
+import italic from '@/assets/icons/ic_italic.svg';
+import underline from '@/assets/icons/ic_underline.svg';
+import arrow from '@/assets/icons/ic_arrow.svg';
+import bullet from '@/assets/icons/ic_bullet.svg';
+import number from '@/assets/icons/ic_number.svg';
+import align from '@/assets/icons/align_left.svg';
+import image from '@/assets/icons/ic_image.svg';
+import video from '@/assets/icons/ic_video.svg';
 
 interface WikiHeaderProps {
   className?: string;
@@ -31,13 +31,13 @@ const WikiHeader = ({
   return (
     <>
       {isEditable ? (
-        <div className={styles["header-container"]}>
-          <div className={styles["user-name"]}>
+        <div className={styles['header-container']}>
+          <div className={styles['user-name']}>
             <span>{profile?.name}</span>
           </div>
-          <div className={styles["editor-container"]}>
-            <div className={styles["editor-wrapper"]}>
-              <button className={styles["editor-btn"]}>
+          <div className={styles['editor-container']}>
+            <div className={styles['editor-wrapper']}>
+              <button className={styles['editor-btn']}>
                 <Image
                   src={bold}
                   width={24}
@@ -45,7 +45,7 @@ const WikiHeader = ({
                   alt="텍스트 굵게 표시 아이콘"
                 />
               </button>
-              <button className={styles["editor-btn"]}>
+              <button className={styles['editor-btn']}>
                 <Image
                   src={italic}
                   width={24}
@@ -53,7 +53,7 @@ const WikiHeader = ({
                   alt="이탤릭체 아이콘"
                 />
               </button>
-              <button className={styles["editor-btn"]}>
+              <button className={styles['editor-btn']}>
                 <Image
                   src={underline}
                   width={24}
@@ -61,12 +61,12 @@ const WikiHeader = ({
                   alt="밑줄 아이콘"
                 />
               </button>
-              <div className={styles["dividing-line"]}></div>
-              <span className={styles["subject"]}>제목</span>
-              <button className={styles["editor-btn"]}>
+              <div className={styles['dividing-line']}></div>
+              <span className={styles['subject']}>제목</span>
+              <button className={styles['editor-btn']}>
                 <Image src={arrow} width={24} height={24} alt="토글 아이콘" />
               </button>
-              <button className={styles["editor-btn"]}>
+              <button className={styles['editor-btn']}>
                 <Image
                   src={bullet}
                   width={24}
@@ -74,7 +74,7 @@ const WikiHeader = ({
                   alt="글머리 아이콘"
                 />
               </button>
-              <button className={styles["editor-btn"]}>
+              <button className={styles['editor-btn']}>
                 <Image
                   src={number}
                   width={24}
@@ -82,11 +82,11 @@ const WikiHeader = ({
                   alt="번호 매기기 아이콘"
                 />
               </button>
-              <button className={styles["editor-btn"]}>
+              <button className={styles['editor-btn']}>
                 <Image src={align} width={24} height={24} alt="정렬 아이콘" />
               </button>
-              <div className={styles["dividing-line"]}></div>
-              <button className={styles["editor-btn"]}>
+              <div className={styles['dividing-line']}></div>
+              <button className={styles['editor-btn']}>
                 <Image
                   src={image}
                   width={24}
@@ -94,8 +94,8 @@ const WikiHeader = ({
                   alt="이미지 첨부 아이콘"
                 />
               </button>
-              <div className={styles["dividing-line"]}></div>
-              <button className={styles["editor-btn"]}>
+              <div className={styles['dividing-line']}></div>
+              <button className={styles['editor-btn']}>
                 <Image
                   src={video}
                   width={24}
@@ -103,7 +103,7 @@ const WikiHeader = ({
                   alt="비디오 첨부 아이콘"
                 />
               </button>
-              <button className={styles["editor-btn"]}>
+              <button className={styles['editor-btn']}>
                 <Image
                   src={link}
                   width={24}
@@ -115,20 +115,20 @@ const WikiHeader = ({
           </div>
         </div>
       ) : (
-        <section className={`${styles["wiki-actions"]} ${className}`}>
-          <section className={styles["name-and-btn"]}>
-            <span className={styles["username"]}>{profile?.name}</span>
+        <section className={`${styles['wiki-actions']} ${className}`}>
+          <section className={styles['name-and-btn']}>
+            <span className={styles['username']}>{profile?.name}</span>
             {hasSections && (
-              <button className={styles["participate-btn"]}>
+              <button className={styles['participate-btn']}>
                 위키 참여하기
               </button>
             )}
           </section>
-          <section className={styles["link"]}>
-            <section className={styles["link-wrapper"]}>
+          <section className={styles['link']}>
+            <section className={styles['link-wrapper']}>
               <Image src={link} width={20} height={20} alt="링크 아이콘" />
-              <Link href="#" className={styles["link-copy-btn"]}>
-                <section className={styles["link-address"]}>
+              <Link href="#" className={styles['link-copy-btn']}>
+                <section className={styles['link-address']}>
                   https://www.wikied.kr/{profile?.code}
                 </section>
               </Link>
