ad04fc9381e497684a83e74ed4a6ebe84014315c
강효성 <goodkingdawn@naver.com>
2024-08-06 21:27:42 +0900

    fix: 렌딩페이지 수정

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -4,9 +4,6 @@ import Button from '@/components/button';
 import Input from '@/components/input';
 
 const LoginPage = () => {
-  /**
-   * @ ChangePasswordInput, AddWikiInput에 공통 컴포넌트 (Button, Input) 적용해야 함
-   */
   return (
     <div className={styles['login-container']}>
       <div className={styles['login-wrapper']}>
@@ -21,7 +18,6 @@ const LoginPage = () => {
             placeholder="이메일을 입력해 주세요"
           ></Input>
         </div>
-
         <div className={styles['password-input-wrapper']}>
           <Input
             label="비밀번호"
@@ -33,7 +29,7 @@ const LoginPage = () => {
           ></Input>
         </div>
       </div>
-      <Button className={styles['login-bts']}>로그인</Button>
+      <Button className={styles['login-bts']} >로그인</Button>
       <Link className={styles['signup']} href="/signup">
         회원가입
       </Link>
