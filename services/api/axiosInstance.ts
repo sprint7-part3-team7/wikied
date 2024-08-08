b4676877387bf4e55635d3e0ce2d0757346ed33c
ddol9 <drddol9@gmail.com>
2024-08-08 19:05:46 +0900

    fix: 불필요한 console 삭제

--- services/api/axiosInstance.ts
+++ services/api/axiosInstance.ts
@@ -10,7 +10,6 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('authToken');
-    console.log('token', token);
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
