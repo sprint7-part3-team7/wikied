58f5fbaa9da93edc9c4b01b069f3d6c068bd3d10
JeongYuseung <dbtmd0925@gmail.com>
2024-08-09 19:07:43 +0900

    feat/ 게시물 작성 페이지 생성

--- pages/addboard/index.tsx
+++ pages/addboard/index.tsx
@@ -1,9 +1,28 @@
 import Editor from '@/components/editor';
+import Button from '@/components/button';
+import { useRouter } from 'next/router';
+import styles from './styles.module.scss';
 
 const AddBoard = () => {
+    const router = useRouter();
+
+    const handleBackButtonClick = () => {
+    router.push('/boards');
+  };
+
   return (
-    <div>
-      <Editor />
+    <div className={styles['add-board-container']}>
+      <div className={styles['editor-container']}>
+        <Editor />
+      </div>
+      <Button
+        color="outline"
+        size="large"
+        className={styles['button-back']}
+        onClick={handleBackButtonClick}
+      >
+        목록으로
+      </Button>
     </div>
   );
 };
