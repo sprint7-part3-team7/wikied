2643b6b7faf4b69fa310b0fb96a6969e97c97394
yongb2n <144973656+yongb2n@users.noreply.github.com>
2024-08-10 14:12:10 +0900

    feat: EditNotification 모달 구현 및 Modal 공통되는 부분 구현 및 적용 (#28)
    * feat: 게스트(로그인을 하지 않은 사용자) 프로필 컴포넌트 생성
    
    * feat: 유저(로그인을 한 사용자) 프로필 컴포넌트 생성
    
    * feat: 헤더 로고 컴포넌트 생성
    
    * feat: 헤더 목록 컴포넌트 생성
    
    * feat: 헤더 모바일 드롭다운 메뉴 컴포넌트 생성
    
    * feat: 헤더 컴포넌트 생성
    
    * feat: _app.tsx에 헤더 컴포넌트 추가
    
    * style: body에 -webkit-font-smoothing 속성 추가
    
    * style: 헤더 max-width > width 변경
    
    * style: 헤더 box-shadow 추가
    
    * rename: import.scss에서 index.scss로 변경
    
    * feat: 공통 버튼 컴포넌트 생성
    
    * feat: 계정 설정 페이지 퍼블리싱 완료
    
    * refactor: import.scss > index.scss 로 변경
    
    * chore: 코드 전체에 prettier 적용
    
    * feat: 이미지 추가
    
    * style: color, font 파일에 변수 추가
    
    * feat: 공통 Modal 컴포넌트 생성
    
    * refactor: 이미지 추가 모달에 공통 모달 컴포넌트 적용
    
    * refactor: Alert 모달에 공통 모달 컴포넌트 적용
    
    * refactor: 퀴즈 모달에 공통 모달 컴포넌트 적용
    
    * feat: EditNotification 모달 생성
    
    * remove: user.ts, wiki.ts 파일 삭제
    
    * style: font 변수 추가
    
    * feat: Modal 컴포넌트에 middle size 추가
    
    * refactor: 코드 리팩토링 (size 변경, 및 폴더 구조 변경)
    
    * refactor: 공통 컴포넌트 모달에 closeIcon 추가 및 리팩토링

---
+++ components/modal/components/addImage/index.tsx
@@ -0,0 +1,83 @@
+import { useState, useRef, ChangeEvent } from 'react';
+import clsx from 'clsx';
+import Image from 'next/image';
+import Button from '@/components/button';
+import styles from '@/components/modal/components/addImage/styles.module.scss';
+import Modal from '../..';
+import closeIcon from '@/assets/icons/ic_close.svg';
+import cameraIcon from '@/assets/icons/ic_camera.svg';
+
+interface AddImageProps {
+  size?: 'middle' | 'large';
+}
+
+const AddImage = ({ size = 'large' }: AddImageProps): JSX.Element => {
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleInputClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        if (typeof reader.result === 'string') {
+          setImagePreview(reader.result);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <Modal size={size}>
+      <div className={clsx(styles['container'], styles[size])}>
+        <div className={styles['image-wrapper']}>
+          <label htmlFor="file-input" className={styles['image-label']}>
+            이미지
+          </label>
+          <div className={styles['image-input-box']} onClick={handleInputClick}>
+            <input
+              id="file-input"
+              type="file"
+              ref={fileInputRef}
+              className={styles['image-input']}
+              onChange={handleImageChange}
+              accept="image/*"
+            />
+            {imagePreview ? (
+              <img
+                src={imagePreview}
+                alt="미리보기"
+                className={styles['preview-image']}
+              />
+            ) : (
+              <Image
+                className={styles['image']}
+                src={cameraIcon}
+                alt="카메라"
+                width={36}
+                height={36}
+              />
+            )}
+          </div>
+        </div>
+        <Button
+          size="small"
+          color="disabled"
+          alignEnd
+          className={styles['button']}
+        >
+          삽입하기
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default AddImage;
