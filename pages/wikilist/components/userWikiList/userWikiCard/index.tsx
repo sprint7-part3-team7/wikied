99c97b0df4965a45f5cda22e7e74f516d60d33cd
JeongYuseung <dbtmd0925@gmail.com>
2024-08-10 22:40:44 +0900

    feat/유저카드 클릭 시 해당 위키페이지로 이동하는 기능 추가

--- pages/wikilist/components/userWikiList/userWikiCard/index.tsx
+++ pages/wikilist/components/userWikiList/userWikiCard/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './styles.module.scss';
 import { ProfileSummary } from '@/types/wiki';
 import DefaultProfileImg from '@/assets/icons/ic_profile.svg';
@@ -9,21 +10,23 @@ interface UserCardProps {
 
 const UserWikiCard = ({ user }: UserCardProps) => {
   return (
-    <div className={styles['user-card']}>
-      <img
-        src={user.image || DefaultProfileImg}
-        alt="프로필 이미지"
-        width={85}
-        height={85}
-        className={styles['profile-image']}
-      />
-      <div className={styles['user-info']}>
-        <div className={styles['user-name']}>{user.name}</div>
-        <div className={styles['user-details']}>
-          <div>{user.city}, {user.nationality}</div>
-          <div>{user.job}</div>
+    <div className={styles['user-card-container']}>
+      <Link className={styles['user-card']} href={`/wiki/${user.code}`}>
+        <img
+          src={user.image || DefaultProfileImg}
+          alt="프로필 이미지"
+          width={85}
+          height={85}
+          className={styles['profile-image']}
+        />
+        <div className={styles['user-info']}>
+          <div className={styles['user-name']}>{user.name}</div>
+          <div className={styles['user-details']}>
+            <div>{user.city}, {user.nationality}</div>
+            <div>{user.job}</div>
+          </div>
         </div>
-      </div>
+      </Link>
       <div className={styles['user-link-container']}>
         <UserLink url={`/wiki/${user.code}`} />
       </div>
