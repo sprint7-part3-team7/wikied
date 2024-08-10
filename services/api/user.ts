6aa612ca716f03cc9ae254a9d7cd4a46f6f7a344
binyongbin <kyb2475@gmail.com>
2024-08-10 17:58:16 +0900

    feat: User API 및 타입 정의

--- services/api/user.ts
+++ services/api/user.ts
@@ -1,39 +1,19 @@
-import axios from 'axios';
-import { User } from '@/types/user';
+import { ChangePasswordRequest, User } from '@/types/user';
+import { authAxiosInstance } from './axiosInstance';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 interface UserResponse {
-  totalCount: number;
-  list: User[];
+  user: User;
 }
 
-export const getUsers = async (
-  page: number,
-  limit: number,
-  search: string,
-): Promise<UserResponse> => {
-  try {
-    const response = await axios.get<UserResponse>(`${API_BASE_URL}/users`, {
-      params: {
-        page,
-        limit,
-        search,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching users:', error);
-    throw error;
-  }
+export const getUsers = () => {
+  return authAxiosInstance.get<UserResponse>('/users/me');
 };
 
-export const getUserDetail = async (id: number): Promise<User> => {
-  try {
-    const response = await axios.get<User>(`${API_BASE_URL}/users/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching user details:', error);
-    throw error;
-  }
+export const changePassword = (requestData: ChangePasswordRequest) => {
+  return authAxiosInstance.patch<UserResponse>(
+    '/users/me/password',
+    requestData,
+  );
 };
