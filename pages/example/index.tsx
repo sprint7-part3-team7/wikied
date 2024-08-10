6ed7e084e9b92ba27125873e1410d38e1bc4f6e0
binyongbin <kyb2475@gmail.com>
2024-08-10 15:14:40 +0900

    feat: 모달 테스트 페이지

--- pages/example/index.tsx
+++ pages/example/index.tsx
@@ -1,15 +1,36 @@
-import styles from '@/pages/example/styles.module.scss';
-import ExampleComponents from './components/exampleComponents';
 import Button from '@/components/button';
+import Modal from '@/components/modal';
+import Alert from '@/components/modal/components/alert';
+import { useState } from 'react';
+
+/**
+ * @ 모달 사용법 (Alert 기준)
+ */
 
 const Example = () => {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const openModal = () => setModalVisible(true);
+  const closeModal = () => setModalVisible(false);
+
   return (
-    <div className={styles['container']}>
-      <Button className="abc" color="primary" size="small" defaultPadding>
-        내 위키 만들기
+    <div>
+      <Button color="primary" size="large" fullWidth onClick={openModal}>
+        모달 열기
       </Button>
-      <p className={styles['fighting']}>7팀 파이팅!!!</p>
-      <ExampleComponents />
+      {isModalVisible && (
+        <Modal
+          size="large"
+          contents={({ size }) => (
+            <Alert
+              title="5분 이상 글을 쓰지 않아 접속이 끊어졌어요."
+              description="위키 참여하기를 통해 다시 위키를 수정해 주세요."
+              content="확인"
+              size={size}
+            />
+          )}
+          onClose={closeModal}
+        />
+      )}
     </div>
   );
 };
