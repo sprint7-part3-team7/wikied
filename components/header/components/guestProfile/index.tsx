ba0c86ca493921c0cee5601d1476bdd3e5fefd64
강효성 <goodkingdawn@naver.com>
2024-08-10 18:01:37 +0900

    feat: 헤더 로그인시 로그아웃 체인지 구현

--- components/header/components/guestProfile/index.tsx
+++ components/header/components/guestProfile/index.tsx
@@ -1,11 +1,26 @@
+import { useAuth } from '@/contexts/AuthProvider';
 import styles from '@/components/header/components/guestProfile/styles.module.scss';
 import Link from 'next/link';
 
 const GuestProfile = () => {
+  const { isLoggedIn, logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
-    <Link href="/login">
-      <button className={styles['login']}>로그인</button>
-    </Link>
+    <>
+      {isLoggedIn ? (
+        <button className={styles['login']} onClick={handleLogout}>
+          로그아웃
+        </button>
+      ) : (
+        <Link href="/login">
+          <button className={styles['login']}>로그인</button>
+        </Link>
+      )}
+    </>
   );
 };
 
