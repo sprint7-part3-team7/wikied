4aa472b2eeb656e957d52cb3630320b5ddfdfd92
kingdawn <166676502+kanghyosung1@users.noreply.github.com>
2024-08-10 14:11:49 +0900

    feat: 로그인 페이지 유효성검사 추가 (#26)
    * fix: 로그인페이지 공통컴포넌트적용
    
    * fix: 회원가입페페이지 공통컴포넌트적용
    
    * fix: 렌딩페이지 수정
    
    * feat: 로그인페이지 유효성 완료
    
    * feat: 회원가입 로그인 기능 완료
    
    * chore: 임시저장
    
    * feat: 로그인 상태관리 context 구현
    
    ---------
    
    Co-authored-by: 강효성 <goodkingdawn@naver.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import Header from '@/components/header';
 import 'contenido/dist/styles.css';
+import { AuthProvider } from "@/contexts/AuthProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Header />
+        <Component {...pageProps} />
+      </AuthProvider>
     </>
   );
 }
