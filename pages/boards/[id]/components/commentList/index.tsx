19c0141456cfe819a6ca3968ae469cab1a47a875
Yewon Choi <118425946+ddol9@users.noreply.github.com>
2024-08-10 14:11:31 +0900

    feat: 게시글 페이지 댓글 기능 추가 (#29)
    * fix: 게시글 댓글 글자수 제한 및 텍스트 겹치는 부분 수정
    
    * feat: 댓글 api 추가
    
    * style: 댓글 padding 수정
    
    * refactor: 댓글 scss color 변수 적용
    
    * feat: 댓글 등록 이벤트 구현
    
    * fix: 댓글이 없을 때 발생하는 예외 처리
    
    * fix: axios token 변수명 수정
    
    * fix: 불필요한 console 삭제
    
    * style: 댓글 Input css 수정
    
    * feat: axiosInstance 로그인 필요 여부에 따라 분리

--- pages/boards/[id]/components/commentList/index.tsx
+++ pages/boards/[id]/components/commentList/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './styles.module.scss';
 import { Comment } from '@/types/article';
 import Image from 'next/image';
@@ -6,35 +6,77 @@ import defaultProfile from '@/assets/icons/ic_profile.svg';
 import editImage from '@/assets/icons/ic_edit.svg';
 import deleteImage from '@/assets/icons/ic_delete.svg';
 import Button from '@/components/button';
+import { postComment } from '@/services/api/comment';
 
 interface CommentListProps {
   comments: Comment[];
+  articleId: number;
 }
 
-const CommentList = ({ comments }: CommentListProps) => {
+const CommentList = ({ comments, articleId }: CommentListProps) => {
   const [newComment, setNewComment] = useState('');
+  const [isCommentAuthor, setIsCommentAuthor] = useState(false);
+  const maxLength = 500;
+
+  useEffect(() => {
+    if (comments.length === 0) {
+      setIsCommentAuthor(false);
+      return;
+    }
+    const currentUserId = localStorage.getItem('userId');
+    setIsCommentAuthor(currentUserId === comments[0].writer.id.toString());
+  }, [comments]);
+
+  const handleChangeComment = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const input = e.target.value;
+    if (input.length <= maxLength) {
+      setNewComment(input);
+    }
+  };
+
+  const handleCommentSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (newComment.trim() === '') {
+      alert('댓글을 입력해 주세요');
+      return;
+    } else {
+      try {
+        await postComment(articleId, newComment);
+        setNewComment('');
+      } catch (error) {
+        console.error(error);
+        alert('댓글 등록 실패');
+      }
+    }
+  };
 
   return (
     <div className={styles['comment-container']}>
       <div className={styles['comment-title']}>
         댓글 <span className={styles['comment-count']}>{comments.length}</span>
       </div>
-
-      <form className={styles['comment-input']}>
+      <form className={styles['comment-input-container']}>
         <textarea
+          className={styles['comment-textarea']}
           value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
+          onChange={handleChangeComment}
           placeholder="댓글을 입력해 주세요"
+          maxLength={maxLength}
         />
-        <Button
-          color="primary"
-          size="large"
-          className={styles['submit-button']}
-        >
-          댓글 등록
-        </Button>
+        <div className={styles['comment-input-footer']}>
+          <span className={styles['character-count']}>
+            {newComment.length}/{maxLength}
+          </span>
+          <Button
+            color="primary"
+            size="large"
+            className={styles['submit-button']}
+            onClick={handleCommentSubmit}
+          >
+            댓글 등록
+          </Button>
+        </div>
       </form>
-
       <div className={styles['comment-list']}>
         {comments.map((comment) => (
           <div key={comment.id} className={styles['comment-box']}>
@@ -60,19 +102,26 @@ const CommentList = ({ comments }: CommentListProps) => {
                 <span className={styles['comment-author-name']}>
                   {comment.writer.name}
                 </span>
-                <div className={styles['comment-button-wrapper']}>
-                  <button className={styles['comment-button']}>
-                    <Image src={editImage} alt="수정" width={24} height={24} />
-                  </button>
-                  <button className={styles['comment-button']}>
-                    <Image
-                      src={deleteImage}
-                      alt="삭제"
-                      width={24}
-                      height={24}
-                    />
-                  </button>
-                </div>
+                {isCommentAuthor && (
+                  <div className={styles['comment-button-wrapper']}>
+                    <button className={styles['comment-button']}>
+                      <Image
+                        src={editImage}
+                        alt="수정"
+                        width={24}
+                        height={24}
+                      />
+                    </button>
+                    <button className={styles['comment-button']}>
+                      <Image
+                        src={deleteImage}
+                        alt="삭제"
+                        width={24}
+                        height={24}
+                      />
+                    </button>
+                  </div>
+                )}
               </div>
               <p className={styles['comment-content']}>{comment.content}</p>
               <span className={styles['comment-date']}>
