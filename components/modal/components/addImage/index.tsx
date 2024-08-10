f2042aaf451740370e81b3777071f436f9ce1ba8
ddol9 <drddol9@gmail.com>
2024-08-10 17:12:36 +0900

    feat: 에디터 사진 추가 모달 연결 및 addImage 모달 수정

--- components/modal/components/addImage/index.tsx
+++ components/modal/components/addImage/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, ChangeEvent } from 'react';
+import React, { useState, useRef, ChangeEvent } from 'react';
 import clsx from 'clsx';
 import Image from 'next/image';
 import Button from '@/components/button';
@@ -7,10 +7,17 @@ import cameraIcon from '@/assets/icons/ic_camera.svg';
 
 interface AddImageProps {
   size?: 'small' | 'large';
+  onImageUpload: (file: File) => void;
+  onClose: () => void;
 }
 
-const AddImage = ({ size = 'large' }: AddImageProps): JSX.Element => {
+const AddImage = ({
+  size = 'large',
+  onImageUpload,
+  onClose,
+}: AddImageProps) => {
   const [imagePreview, setImagePreview] = useState<string | null>(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   const handleInputClick = () => {
@@ -22,6 +29,7 @@ const AddImage = ({ size = 'large' }: AddImageProps): JSX.Element => {
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
+      setSelectedFile(file);
       const reader = new FileReader();
       reader.onloadend = () => {
         if (typeof reader.result === 'string') {
@@ -32,46 +40,58 @@ const AddImage = ({ size = 'large' }: AddImageProps): JSX.Element => {
     }
   };
 
+  const handleInsert = () => {
+    if (selectedFile) {
+      onImageUpload(selectedFile);
+    }
+  };
+
   return (
-    <div className={clsx(styles['container'], styles[size])}>
-      <div className={styles['image-wrapper']}>
-        <label htmlFor="file-input" className={styles['image-label']}>
-          이미지
-        </label>
-        <div className={styles['image-input-box']} onClick={handleInputClick}>
-          <input
-            id="file-input"
-            type="file"
-            ref={fileInputRef}
-            className={styles['image-input']}
-            onChange={handleImageChange}
-            accept="image/*"
-          />
-          {imagePreview ? (
-            <img
-              src={imagePreview}
-              alt="미리보기"
-              className={styles['preview-image']}
-            />
-          ) : (
-            <Image
-              className={styles['image']}
-              src={cameraIcon}
-              alt="카메라"
-              width={36}
-              height={36}
-            />
-          )}
-        </div>
-      </div>
-      <Button
-        size="small"
-        color="disabled"
-        alignEnd
-        className={styles['button']}
+    <div className={styles['modal-overlay']} onClick={onClose}>
+      <div
+        className={clsx(styles['container'], styles[size])}
+        onClick={(e) => e.stopPropagation()}
       >
-        삽입하기
-      </Button>
+        <div className={styles['image-wrapper']}>
+          <label htmlFor="file-input" className={styles['image-label']}>
+            이미지
+          </label>
+          <div className={styles['image-input-box']} onClick={handleInputClick}>
+            <input
+              id="file-input"
+              type="file"
+              ref={fileInputRef}
+              className={styles['image-input']}
+              onChange={handleImageChange}
+              accept="image/*"
+            />
+            {imagePreview ? (
+              <img
+                src={imagePreview}
+                alt="미리보기"
+                className={styles['preview-image']}
+              />
+            ) : (
+              <Image
+                className={styles['image']}
+                src={cameraIcon}
+                alt="카메라"
+                width={36}
+                height={36}
+              />
+            )}
+          </div>
+        </div>
+        <Button
+          size="small"
+          color={selectedFile ? 'primary' : 'disabled'}
+          alignEnd
+          className={styles['button']}
+          onClick={handleInsert}
+        >
+          삽입하기
+        </Button>
+      </div>
     </div>
   );
 };
