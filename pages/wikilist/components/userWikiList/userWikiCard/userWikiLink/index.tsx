abfaf5afbfa7e42839d7b7cccdb98a0c26b1042a
OurIsland <167041475+OurIsland@users.noreply.github.com>
2024-08-11 22:30:47 +0900

    feat: 위키리스트 페이지 css수정 및 기능 추가 (#37)
    * feat/wikilist페이지의 usercard제작 후 ui 제작
    
    * feat/ UserCard컴포넌트 개선 & UserLink 컴포넌트 제작
    
    * refactor/use kebab-case to className
    
    * feat/create notFound component
    
    * camelCase를 따른 컴포넌트 명칭 변경
    
    * refactor/유저위키링크 링크복사기능 추가
    
    * feat/사용자 검색 시 검색결과 문구 표시 기능 추가
    
    * feat/유저카드 클릭 시 해당 위키페이지로 이동하는 기능 추가

--- pages/wikilist/components/userWikiList/userWikiCard/userWikiLink/index.tsx
+++ pages/wikilist/components/userWikiList/userWikiCard/userWikiLink/index.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import styles from './styles.module.scss';
 import LinkIcon from '@/assets/icons/ic_link.svg';
 import Image from 'next/image';
+import SnackBar from '@/components/snackbar';
 
 interface UserLinkProps {
   url: string;
 }
 
 const UserWikiLink = ({ url }: UserLinkProps) => {
+  const [showSnackBar, setShowSnackBar] = useState(false);
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(url)
+      .then(() => {
+        setShowSnackBar(true);
+      })
+      .catch(err => {
+        console.error('링크 복사에 실패했습니다.', err);
+      });
+  };
+
+  useEffect(() => {
+    if (showSnackBar) {
+      const timer = setTimeout(() => {
+        setShowSnackBar(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [showSnackBar]);
+
   return (
-    <div className={styles['link-container']}>
-      <Image src={LinkIcon} alt="Link icon" width={20} height={20} />
-      <a
-        href={url}
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles['link-text']}
-      >
-        {url}
-      </a>
+    <div className={styles['container']}>
+      {showSnackBar && (
+        <div className={styles['snackbar-wrapper']}>
+          <SnackBar
+            message="내 위키 링크가 복사되었습니다."
+            type="success"
+            size="small"
+          />
+        </div>
+      )}
+      <div className={styles['link-container']} onClick={copyToClipboard}>
+        <Image src={LinkIcon} alt="링크" width={20} height={20} />
+        <span className={styles['link-text']}>{url}</span>
+      </div>
     </div>
   );
 };
+
 
 export default UserWikiLink;
