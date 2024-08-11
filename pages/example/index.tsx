98d11b66355b1c130b10c5c77e2da9c9300211cc
binyongbin <kyb2475@gmail.com>
2024-08-11 21:18:15 +0900

    refactor: 토스트 컴포넌트 사용 예시로 리팩토링

--- pages/example/index.tsx
+++ pages/example/index.tsx
@@ -1,37 +1,33 @@
-import Button from '@/components/button';
-import Modal from '@/components/modal';
-import Alert from '@/components/modal/components/alert';
+import Toast from '@/components/toast';
 import { useState } from 'react';
 
 /**
- * @ 모달 사용법 (Alert 기준)
+ * @ 토스트 컴포넌트 사용법 (
  */
 
 const Example = () => {
-  const [isModalVisible, setModalVisible] = useState(false);
-  const openModal = () => setModalVisible(true);
-  const closeModal = () => setModalVisible(false);
+  const [isToastVisible, setIsToastVisible] = useState(false);
+
+  const handleShowToast = () => {
+    setIsToastVisible(true);
+  };
+
+  const handleCloseToast = () => {
+    setIsToastVisible(false);
+  };
 
   return (
-    <div>
-      <Button color="primary" size="large" fullWidth onClick={openModal}>
-        모달 열기
-      </Button>
-      {isModalVisible && (
-        <Modal
-          size="large"
-          contents={({ size }) => (
-            <Alert
-              title="5분 이상 글을 쓰지 않아 접속이 끊어졌어요."
-              description="위키 참여하기를 통해 다시 위키를 수정해 주세요."
-              content="확인"
-              size={size}
-            />
-          )}
-          onClose={closeModal}
+    <>
+      <button onClick={handleShowToast}>토스트 UI 보기</button>
+      {isToastVisible && (
+        <Toast
+          message="로그인에 성공했습니다!"
+          type="success"
+          duration={3000}
+          onClose={handleCloseToast}
         />
       )}
-    </div>
+    </>
   );
 };
 
