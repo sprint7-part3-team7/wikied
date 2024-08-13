f13831a079dbb040abd667a06619ff5393a65ea9
hayuri1990 <hayuri1990@gmail.com>
2024-08-13 09:20:54 +0900

    fix: 참여 가능 시 링크 복사 안되는 문제 해결

--- components/wiki/wikiHeader/index.tsx
+++ components/wiki/wikiHeader/index.tsx
@@ -31,7 +31,10 @@ const WikiHeader = ({
     'success',
   );
   const [showSnackBar, setShowSnackBar] = useState<boolean>(false);
-  const [showLinkCopyModal, setShowLinkCopyModal] = useState<boolean>(false);
+  const [showLinkCopySnackBar, setShowLinkCopySnackBar] =
+    useState<boolean>(false);
+  const [showEditingSnackBar, setShowEditingSnackBar] =
+    useState<boolean>(false);
 
   const handleCopyClick = () => {
     const linkToCopy = `https://www.wikied.kr/${profile.code}`;
@@ -41,13 +44,13 @@ const WikiHeader = ({
         setSnackBarMessage('내 위키 링크가 복사되었습니다.');
         setSnackBarType('success');
         setShowSnackBar(true);
-        setShowLinkCopyModal(true);
+        setShowLinkCopySnackBar(true);
       })
       .catch(() => {
         setSnackBarMessage('복사에 실패했습니다.');
         setSnackBarType('error');
         setShowSnackBar(true);
-        setShowLinkCopyModal(true);
+        setShowLinkCopySnackBar(true);
       });
   };
 
@@ -57,13 +60,14 @@ const WikiHeader = ({
     );
     setSnackBarType('error');
     setShowSnackBar(true);
+    setShowEditingSnackBar(true);
   };
 
   useEffect(() => {
     if (showSnackBar) {
       const timer = setTimeout(() => {
         setShowSnackBar(false);
-        setShowLinkCopyModal(false);
+        setShowLinkCopySnackBar(false);
       }, 3000);
 
       return () => clearTimeout(timer);
