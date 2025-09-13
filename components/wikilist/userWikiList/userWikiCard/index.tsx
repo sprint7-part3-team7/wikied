f6a5ced558421a019accc80a010853c8fa7cc825
hayuri1990 <hayuri1990@gmail.com>
2025-09-13 11:15:14 +0900

    style: 불필요한 주석 삭제

--- components/wikilist/userWikiList/userWikiCard/index.tsx
+++ components/wikilist/userWikiList/userWikiCard/index.tsx
@@ -15,7 +15,6 @@ const UserWikiCard = ({ user }: UserCardProps) => {
       <Link className={styles['user-card-link']} href={`/wiki/${user.code}`}>
         <div className={styles['user-card']}>
           <div className={styles['user-profile-image-container']}>
-            {/* 프로필 이미지 */}
             {user.image ? (
               <Image
                 src={user.image}
@@ -34,7 +33,6 @@ const UserWikiCard = ({ user }: UserCardProps) => {
               />
             )}
           </div>
-          {/* 닉네임 및 정보 */}
           <div className={styles['user-info']}>
             <div className={styles['user-name']}>{user.name}</div>
             <div className={styles['user-details']}>
