f9622ebff8321f2cdb995d14a534539fe117e5ed
hayuri1990 <hayuri1990@gmail.com>
2025-09-14 04:44:43 +0900

    refactor: 페이지네이션 그룹 단위 이동 및 화면 너비에 따른 버튼 개수 조절

--- components/common/pagination/index.tsx
+++ components/common/pagination/index.tsx
@@ -2,6 +2,7 @@ import styles from '@/components/common/pagination/styles.module.scss';
 import rightArrow from '@/assets/icons/pg_right.svg';
 import leftArrow from '@/assets/icons/pg_left.svg';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 interface PaginationProps {
   currentPage: number;
@@ -14,21 +15,48 @@ const Pagination = ({
   totalPages,
   onPageChange,
 }: PaginationProps) => {
+  // 화면 크기 측정
+  const [clientWidth, setClientWidth] = useState<number>(0);
+  const [pageGroupSize, setPageGroupSize] = useState<number>(5); // 한번에 보여줄 페이지 수
+
+  useEffect(() => {
+    const handleResize = () =>
+      setClientWidth(document.documentElement.clientWidth);
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  // 화면 크기에 따라 페이지 그룹 사이즈 변경
+  useEffect(() => {
+    if (clientWidth >= 330) setPageGroupSize(5);
+    else if (clientWidth > 280) setPageGroupSize(4);
+    else if (clientWidth > 230) setPageGroupSize(3);
+    else if (clientWidth > 180) setPageGroupSize(2);
+    else if (clientWidth > 130) setPageGroupSize(1);
+    else if (clientWidth > 80) setPageGroupSize(0);
+  }, [clientWidth]);
+
+  // 그룹 단위 계산
+  const currentGroup = Math.floor((currentPage - 1) / pageGroupSize); // 현재 페이지가 속한 그룹 번호
+  const startPage = currentGroup * pageGroupSize + 1; // 현재 그룹의 시작 페이지
+  const endPage = Math.min(startPage + pageGroupSize - 1, totalPages); // 현재 그룹의 마지막 페이지
+
   const renderPageNumbers = () => {
     const pageNumbers = [];
-    const startPage = Math.max(currentPage - 2, 1);
-    const endPage = Math.min(startPage + 4, totalPages);
 
-    pageNumbers.push(
-      <button
-        key="prev"
-        onClick={() => onPageChange(currentPage - 1)}
-        disabled={currentPage === 1}
-      >
-        <Image src={leftArrow} alt="prev" />
-      </button>,
-    );
+    // 이전 그룹 버튼
+    if (currentGroup > 0) {
+      pageNumbers.push(
+        <button key="prev" onClick={() => onPageChange(startPage - 1)}>
+          <Image src={leftArrow} alt="prev" />
+        </button>,
+      );
+    }
 
+    // 페이지 번호
     for (let i = startPage; i <= endPage; i++) {
       pageNumbers.push(
         <button
@@ -41,19 +69,16 @@ const Pagination = ({
       );
     }
 
-    pageNumbers.push(
-      <button
-        key="next"
-        onClick={() => onPageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
-      >
-        <Image src={rightArrow} alt="next" />
-      </button>,
-    );
-
+    // 다음 그룹 버튼
+    if (endPage < totalPages) {
+      pageNumbers.push(
+        <button key="next" onClick={() => onPageChange(endPage + 1)}>
+          <Image src={rightArrow} alt="next" />
+        </button>,
+      );
+    }
     return pageNumbers;
   };
-
   return <div className={styles['pagination']}>{renderPageNumbers()}</div>;
 };
 
