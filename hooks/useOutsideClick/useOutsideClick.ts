8516cd51308ad8a1c2afd72433c891733940874d
hayuri1990 <hayuri1990@gmail.com>
2025-09-16 13:42:31 +0900

    refactor: Header 컴포넌트의 각 로직을 훅으로 분리

---
+++ hooks/useOutsideClick/useOutsideClick.ts
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+export const useOutsideClick = (
+  ref: React.RefObject<HTMLElement>,
+  onClickOutside: () => void,
+) => {
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        onClickOutside();
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref, onClickOutside]);
+};
