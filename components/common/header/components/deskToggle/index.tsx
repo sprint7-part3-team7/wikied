542aa54723c515eb1ea1a60551f77cc62ac81b5f
hayuri1990 <hayuri1990@gmail.com>
2025-09-16 10:16:17 +0900

    fix: 메뉴 항목 클릭 시 메뉴가 닫히지 않는 문제 해결

--- components/common/header/components/deskToggle/index.tsx
+++ components/common/header/components/deskToggle/index.tsx
@@ -4,7 +4,13 @@ import useWikiNavigation from '@/hooks/useCode/useCode';
 import { useAuth } from '@/contexts/AuthProvider';
 import { useRouter } from 'next/router';
 
-const DeskMenu = () => {
+type DeskMenuProps = {
+  deskMenu: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  toggleMenu: () => void;
+  setModalSize: React.Dispatch<React.SetStateAction<'small' | 'large'>>;
+};
+
+const DeskMenu = ({ deskMenu, toggleMenu, setModalSize }: DeskMenuProps) => {
   const menuRef = useRef<HTMLDivElement>(null);
   const { logout } = useAuth();
   const { handleNavigationWiki } = useWikiNavigation();
@@ -18,21 +24,29 @@ const DeskMenu = () => {
     <div className={styles['container']} ref={menuRef}>
       <button
         className={styles['menu-list']}
-        onClick={() => {
+        onClick={(e) => {
           router.push('/mypage');
+          deskMenu(e);
         }}
       >
         계정 설정
       </button>
       <button
         className={styles['menu-list']}
-        onClick={() => {
+        onClick={(e) => {
           handleNavigationWiki();
+          deskMenu(e);
         }}
       >
         내 위키
       </button>
-      <button className={styles['menu-list']} onClick={handleLogoutClick}>
+      <button
+        className={styles['menu-list']}
+        onClick={(e) => {
+          handleLogoutClick();
+          deskMenu(e);
+        }}
+      >
         로그아웃
       </button>
     </div>
